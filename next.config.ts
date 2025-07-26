f3c34b47f6d76fc53899cbd0e9e8b39949658126
shubham-sahoo <shubhamsomnath@gmail.com>
2025-07-27 01:43:44 +0530

    New config

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,18 @@
+// next.config.js (or .ts if using TypeScript)
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const isGithubActions = !!process.env.GITHUB_ACTIONS;
 
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'shubham-sahoo.github.io';
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
+const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] || "";
 
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  assetPrefix: assetPrefix, // Ensure it starts with a leading slash
-  basePath: basePath, // Base path for GitHub Pages
-  trailingSlash: true, // Ensure paths have trailing slashes
+  assetPrefix: isGithubActions ? `/${repo}/` : "",
+  basePath: isGithubActions ? `/${repo}` : "",
 };
 
-// export default nextConfig;
 module.exports = nextConfig;
